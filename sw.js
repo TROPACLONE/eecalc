45737fe37fc68780abab3a60af62cbe71fd3167f
TROPACLONE <137171547+TROPACLONE@users.noreply.github.com>
2026-09-24 00:02:33 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // Offline support: caches only the app's own files (never user data). Cache-first; a new version
 // (new hash below, set by the build) replaces the old cache on the next launch.
-const CACHE = 'eecalc-560aa46d96c3';
-const FILES = ['./', './index.html', './app.js', './manifest.webmanifest', './icon-180.png', './icon-192.png', './icon-512.png'];
+const CACHE = 'eecalc-549c1b31a13e';
+const FILES = ["./","./app.js","./c-KJR2GYOI.js","./c-KPBPKWEZ.js","./c-PQAVO6OB.js","./c-X3YFTYMA.js","./icon-180.png","./icon-192.png","./icon-512.png","./index.html","./manifest.webmanifest"];
 self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES.map(f => new Request(f, { cache: 'reload' })))).then(() => self.skipWaiting())));
 self.addEventListener('activate', e => e.waitUntil(
   caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())));
